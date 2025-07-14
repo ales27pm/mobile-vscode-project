84514c776df451c0b09c1a2e2b0381790be7279f
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-07-14 00:29:46 -0400

    Add backend tests

---
+++ apps/backend/src/types/test-shims.d.ts
@@ -0,0 +1,13 @@
+declare module 'yjs' {
+    namespace Y {
+        interface Doc {
+            on: any;
+        }
+    }
+    const Y: any;
+    export = Y;
+}
+declare module 'lodash.debounce' {
+    const fn: any;
+    export default fn;
+}
