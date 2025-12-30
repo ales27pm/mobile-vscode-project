8bddee0cab2532216e859bef9fabe65562a4b1c7
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-12-30 00:52:56 -0500

    Update https.ts

--- apps/backend/__mocks__/https.ts
+++ apps/backend/__mocks__/https.ts
@@ -4,12 +4,11 @@ const listen = jest.fn((port: number, hostOrCb: any, maybeCb?: any) => {
 });
 
 const close = jest.fn((cb?: () => void) => {
-  if (cb) cb();
+  if (cb) {
+    cb();
+  }
 });
-
 const on = jest.fn();
 const address = jest.fn(() => ({ address: '127.0.0.1', port: 4000 }));
-
 export const __mocks = { listen, close, on, address };
-
 export const createServer = jest.fn(() => ({ listen, close, on, address }));
